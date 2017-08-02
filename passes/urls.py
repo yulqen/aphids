3157e13f00b10ea5ca9d7b94c5eec9052069cc81
Matthew Lemon <lemon@matthewlemon.com>
2017-08-02 14:39:35 +0100

    fix for url

--- passes/urls.py
+++ passes/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from passes import views
 
 urlpatterns = [
-    url(r'^passes/pass-status/$', views.pass_status_list),
-    url(r'^passes/pass-status/(?P<pk>[0-9]+)/$', views.pass_status_detail),
+    url(r'^pass-status/$', views.pass_status_list),
+    url(r'^pass-status/(?P<pk>[0-9]+)/$', views.pass_status_detail),
 ]
