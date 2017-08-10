80a4ea43a429237587dece2deafba15f36628e48
Matthew Lemon <lemon@matthewlemon.com>
2017-08-10 15:37:48 +0100

    added rest_framework magic to allow logging in

--- aphids_api/urls.py
+++ aphids_api/urls.py
@@ -18,3 +18,8 @@ from django.conf.urls import url, include
 urlpatterns = [
     url(r'^', include('passes.urls')),
 ]
+
+urlpatterns += [
+    url(r'^api-auth/', include('rest_framework.urls',
+                               namespace='rest_framework')),
+]
