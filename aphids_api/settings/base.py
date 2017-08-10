d2caeaa2f3663d949bc9cf73d249a93534fe5c3e
Matthew Lemon <lemon@matthewlemon.com>
2017-08-10 13:40:56 +0100

    changed DEFAULT_AUTHENTICATION_CLASSES to BasicAuthentication

--- aphids_api/settings/base.py
+++ aphids_api/settings/base.py
@@ -47,6 +47,9 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAdminUser',
     ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.BasicAuthentication',
+    )
 }
 
 MIDDLEWARE = [
