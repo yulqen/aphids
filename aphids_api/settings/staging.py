70e1942c73160877f92d63bad01b2fb17796e44f
Matthew Lemon <lemon@matthewlemon.com>
2017-08-11 16:43:36 +0100

    implementing change in template and static files

--- aphids_api/settings/staging.py
+++ aphids_api/settings/staging.py
@@ -6,3 +6,12 @@ db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
 ALLOWED_HOSTS = ['salty-tor-62302.herokuapp.com']
+
+DEBUG = False
+
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_URL = '/static/'
+
+STATICFILE_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
