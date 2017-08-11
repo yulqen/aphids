a9c80aa26eef0c6eb89de6d8ca611a08f7e55d86
Matthew Lemon <lemon@matthewlemon.com>
2017-08-11 20:47:52 +0100

    reconfigured BASE_DIR using Path a la Two Scoops and moved template folder

--- aphids_api/settings/base.py
+++ aphids_api/settings/base.py
@@ -69,7 +69,7 @@ ROOT_URLCONF = 'aphids_api.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
