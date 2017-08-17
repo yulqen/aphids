d3d07a06d8859e78626a7da0e04c6759c1a86a4a
Matthew Lemon <lemon@matthewlemon.com>
2017-08-17 12:34:26 +0100

    trying change in urls.py

--- aphids_api/urls.py
+++ aphids_api/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^api/', include('passes.urls')),
@@ -23,3 +25,6 @@ urlpatterns += [
     url(r'^api-auth/', include('rest_framework.urls',
                                namespace='rest_framework')),
 ]
+
+if not settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
