fa876b5fc2cb5f3abcf4ee31867efc76bb05b646
Matthew Lemon <lemon@matthewlemon.com>
2017-08-17 13:25:06 +0100

    fix in settings - again

--- aphids_api/settings/staging.py
+++ aphids_api/settings/staging.py
@@ -25,7 +25,7 @@ STATIC_ROOT = BASE_DIR / 'static_root'
 
 # AWS settings
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-STATIC_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 AWS_STORAGE_BUCKET_NAME = 'aphids-test'
 AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
 AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
