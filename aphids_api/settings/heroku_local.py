9fdf13b88293e2523ed85d2c2a19ed60f575f1ed
Matthew Lemon <lemon@matthewlemon.com>
2017-08-18 11:50:47 +0100

    removed .heroku_env file as it now contains AWS keys

--- aphids_api/settings/heroku_local.py
+++ aphids_api/settings/heroku_local.py
@@ -1,5 +1,9 @@
 # settings/heroku_local.py
 
 from .staging import *
+import os
 
 ALLOWED_HOSTS = ['localhost', '0.0.0.0']
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+DEBUG=True
