82548b0b49a39f36eb8bb25fde6bc2d09c107da3
Matthew Lemon <lemon@matthewlemon.com>
2017-09-29 14:31:19 +0100

    revised test - using APIClient

--- tests/test_api.py
+++ tests/test_api.py
@@ -3,41 +3,30 @@ import json
 from django.test import TestCase
 from django.urls import reverse
 from django.contrib.auth.models import User
-from rest_framework.authtoken.models import Token
-from rest_framework.test import force_authenticate
-from rest_framework.test import APIRequestFactory
+from rest_framework.test import APIClient
 
 from passes.models import PassType
-from passes.views import PassTypeList
+from passes.views import PassTypeList, PassTypeDetail
 
 
 class DjangoRestFrameworkTests(TestCase):
 
     def setUp(self):
-        self.factory = APIRequestFactory()
-        User.objects.get_or_create(username='test_lemon', password='test')
-        for user in User.objects.all():
-            Token.objects.get_or_create(user=user)
+
+        User.objects.create_user(username='test_lemon', password='lemon')
+        self.client = APIClient()
+        self.client.login(username='test_lemon', password='lemon')
+
         PassType.objects.get_or_create(pass_type='The Gold Standard!')
         PassType.objects.get_or_create(pass_type='The Silver Standard!')
-        self.user = User.objects.get(id=1)
-        self.view = PassTypeList.as_view()
 
         self.create_read_url = reverse('pass-type-list')
-        self.read_update_delete_url = reverse('pass-type-detail', args=[1])
+        self.read_update_delete_url = reverse('pass-type-detail', kwargs={'pk': 1})
 
 
     def test_list(self):
-        request = self.factory.get(self.create_read_url)
-        force_authenticate(request, user=self.user)
-        response = self.view(request)
+        response = self.client.get('/api/pass-type/')
 
         # Are both types in content?
         self.assertContains(response, 'The Gold Standard!')
         self.assertContains(response, 'The Silver Standard!')
-
-#    def test_detail(self):
-#        response = self.client.get(self.read_update_delete_url)
-#        data = json.loads(response.content)
-#        content = {'id': 1, 'pass_type': 'The Gold Standard!'}
-#        self.assertEquals(data, content)
